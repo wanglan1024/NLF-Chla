3b58f06356040b03d04c2132097c50874a7a673c
wanglan1024 <wanglan@cque.edu.cn>
2025-11-18 12:33:25 +0800

    Add files via upload

---
+++ NLF/src/recommender/common/RTuple.java
@@ -0,0 +1,14 @@
+package recommender.common;
+
+public class RTuple {
+	
+	public int iUserID = 0;
+	
+	public int iItemID = 0;
+	
+	public double dRating = 0;
+	
+	public double dRatingHat = 0;
+	
+	public double lTimeStamp = 0;				
+}
